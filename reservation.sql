a53d1fb34b553f9d356163c212af33d483a9240b
KyeongJun_LEE <113089467+github-ekyungjune@users.noreply.github.com>
2023-02-28 13:54:17 +0900

    Update reservation.sql

--- reservation.sql
+++ reservation.sql
@@ -2,9 +2,9 @@
 CREATE TABLE customer (id INT(50), name VARCHAR(50), birthday DATE, phone VARCHAR(50));
 
 #2. customer 테이블에 값을 넣기
-INSERT INTO movie (id, name, birthday, phone) VALUES ('1','이경준','2000-10-09','010-7660-5380');
-INSERT INTO movie (id, name, birthday, phone) VALUES ('2','김현민','2000-09-30','010-4528-1475');
-INSERT INTO movie (id, name, birthday, phone) VALUES ('3','최진성','2000-09-04','010-1624-1596');
+INSERT INTO customer (id, name, birthday, phone) VALUES ('1','이경준','2000-10-09','010-7660-5380');
+INSERT INTO customer (id, name, birthday, phone) VALUES ('2','김현민','2000-09-30','010-4528-1475');
+INSERT INTO customer (id, name, birthday, phone) VALUES ('3','최진성','2000-09-04','010-1624-1596');
 
 #3. customer 테이블 값 출력
 SELECT * FROM customer;
